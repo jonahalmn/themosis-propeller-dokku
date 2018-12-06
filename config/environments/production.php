8cde73de5a23dc9a3fde7dd9f1346322dc7d2770
jonahalmn <jonah.allemonne@icloud.com>
2018-12-06 19:10:31 +0100

    database host

--- config/environments/production.php
+++ config/environments/production.php
@@ -4,10 +4,10 @@
 // Production config
 /*----------------------------------------------------*/
 // Database
-define('DB_NAME', getenv('DOKKU_MARIADB_THEMOSISDB_NAME'));
+define('DB_NAME', getenv('themosisdb'));
 define('DB_USER', getenv('DOKKU_MARIADB_THEMOSISDB_ENV_MYSQL_USER'));
 define('DB_PASSWORD', getenv('DOKKU_MARIADB_THEMOSISDB_ENV_MYSQL_PASSWORD'));
-define('DB_HOST', getenv('DOKKU_MARIADB_THEMOSISDB_PORT_3306_TCP_ADDR'));
+define('DB_HOST', 'dokku-mariadb-themosisdb');
 
 // WordPress URLs
 define('WP_HOME', getenv('WP_HOME'));
