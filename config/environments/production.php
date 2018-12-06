23fd4c2a407dab3b48bbf818f74d375c07d1d06c
jonahalmn <jonah.allemonne@icloud.com>
2018-12-06 17:07:00 +0100

    mariadb .env vars

--- config/environments/production.php
+++ config/environments/production.php
@@ -4,39 +4,27 @@
 // Production config
 /*----------------------------------------------------*/
 // Database
-
-$url = getenv('DATABASE_URL');
-$components = parse_url($url);
-
-if ($components) {
- $host = $components['host'];
- $username = $components['user'];
- $password = $components['pass'];
- $dbname = substr($components['path'], 1);
- $port = $components['port'];
-
-define('DB_NAME', $dbname);
-define('DB_USER', $username);
-define('DB_PASSWORD', $password);
-define('DB_HOST', $host ? $host : 'localhost');
-}
+define('DB_NAME', getenv('DOKKU_MARIADB_THEMOSISDB_ENV_MYSQL_DATABASE'));
+define('DB_USER', getenv('DOKKU_MARIADB_THEMOSISDB_ENV_MYSQL_USER'));
+define('DB_PASSWORD', getenv('DOKKU_MARIADB_THEMOSISDB_ENV_MYSQL_PASSWORD'));
+define('DB_HOST', getenv('DOKKU_MARIADB_THEMOSISDB_PORT_3306_TCP_ADDR') ? getenv('DOKKU_MARIADB_THEMOSISDB_PORT_3306_TCP_ADDR') : 'localhost');
 
 // WordPress URLs
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
 // Jetpack
-define('JETPACK_DEV_DEBUG', true);
+define('JETPACK_DEV_DEBUG', false);
 
 // Encoding
 define('THEMOSIS_CHARSET', 'UTF-8');
 
 // Development
 define('SAVEQUERIES', false);
-define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', true);
-define('SCRIPT_DEBUG', true);
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+define('SCRIPT_DEBUG', false);
 
 // Themosis framework
-define('THEMOSIS_ERROR', true);
+define('THEMOSIS_ERROR', false);
 define('BS', false);
