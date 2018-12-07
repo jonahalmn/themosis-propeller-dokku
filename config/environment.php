40337e0c866dc9a5bd33d718370c07ddf2691bd7
jonahalmn <jonah.allemonne@icloud.com>
2018-12-07 12:01:58 +0100

    environnement detect

--- config/environment.php
+++ config/environment.php
@@ -4,7 +4,7 @@
 // Define environment type
 /*----------------------------------------------------*/
 return function(){
-    if( getenv('DOKKU_APP_NAME') ) {
+    if( getenv('DOKKU_APP_NAME' !== false) ) {
         return 'production';
     }
 
